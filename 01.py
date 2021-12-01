cff83e026c86b0f8df1b27417656e336f240bce4
Matti <matti.schneider@posteo.de>
2021-12-01 22:30:32 +0100

    1

---
+++ 01.py
@@ -0,0 +1,6 @@
+with open("01.txt", 'r') as f:
+    d = [int(l) for l in f.readlines()]
+
+print('part 1:', sum(1 for i in range(1, len(d)) if d[i] > d[i-1]))
+print('part 2:', sum(1 for i in range(1, len(d))
+      if sum(d[i:i+3]) > sum(d[i-1:i+2])))
