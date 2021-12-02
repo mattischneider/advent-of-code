bcbb49baedcc554f1b349372cfafb7f108eaf62b
Matti <matti.schneider@posteo.de>
2021-12-02 18:38:45 +0100

    2

---
+++ 02.py
@@ -0,0 +1,20 @@
+with open("02.txt", 'r') as f:
+    planned_course = [l.split(' ') for l in f.readlines()]
+
+hor_pos = sum(int(d[1]) for d in planned_course if d[0] == 'forward')
+ups = sum(int(d[1]) for d in planned_course if d[0] == 'up')
+downs = sum(int(d[1]) for d in planned_course if d[0] == 'down')
+
+print('part1', hor_pos * (downs-ups))
+
+aim = 0
+depth = 0
+for d in planned_course:
+    if d[0] == 'up':
+        aim -= int(d[1])
+    if d[0] == 'down':
+        aim += int(d[1])
+    if d[0] == 'forward':
+        depth += int(d[1]) * aim
+
+print('part2', hor_pos * depth)
