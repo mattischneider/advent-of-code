262ba28ae75944c337f72f229a40f40e4150be28
Matti <matti.schneider@posteo.de>
2021-12-13 08:48:24 +0100

    13

---
+++ 2021/13.py
@@ -0,0 +1,54 @@
+import parse
+
+
+def fold(dots: list[list[int]], along_axis: str, coord: int) -> list[list[int]]:
+    out = []
+    if along_axis == 'y':
+        for x, y in dots:
+            if y > coord:
+                out.append((x, coord-(y-coord)))
+            if y == coord:
+                continue
+            if y < coord:
+                out.append((x, y))
+    if along_axis == 'x':
+        for x, y in dots:
+            if x > coord:
+                out.append((coord-(x-coord), y))
+            if x == coord:
+                continue
+            if x < coord:
+                out.append((x, y))
+    return out
+
+
+def fold_all_instrutions(dots: list[list[int]], fold_instrutions: list[tuple[str, int]]):
+    out = dots
+    first_fold = True
+    for a, c in fold_instrutions:
+        out = fold(out, a, c)
+        if first_fold:
+            print('part1:', len(set(out)))
+            first_fold = False
+    return out
+
+
+def print_dots(dots: list[list[int]]):
+    size = max(x for x, _ in dots)
+    print('part2:')
+    for y in range(size):
+        line = ['#' if (x, y) in dots else ' ' for x in range(size)]
+        if '#' in line:
+            print(''.join(line))
+
+
+if __name__ == '__main__':
+    with open("2021/13.txt", 'r') as f:
+        dots_raw, fold_raw = f.read().split('\n\n')
+        dot_positions = [[int(x) for x in t.split(',')]
+                         for t in dots_raw.split('\n')]
+        fold_instrutions = [(d['fold_direction'], d['fold_coord'])
+                            for d in parse.findall('fold along {fold_direction}={fold_coord:d}', fold_raw)]
+
+    fold_result = fold_all_instrutions(dot_positions, fold_instrutions)
+    print_dots(fold_result)
