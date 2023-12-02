ce672f2257a205d85fb7dd42290f26f4e164fa15
Matti <matti.schneider@posteo.de>
2023-12-02 16:06:25 +0100

    2023/02 power snow games

--- 2023/02.py
+++ 2023/02.py
@@ -1,6 +1,7 @@
 import parse
 from dataclasses import dataclass
 from collections import Counter
+from functools import reduce
 
 
 @dataclass()
@@ -13,6 +14,17 @@ class Game:
             if c["red"] > 12 or c["green"] > 13 or c["blue"] > 14:
                 return False
         return True
+
+    def get_minimal_set(self) -> Counter:
+        out = Counter()
+        for g in self.cubes:
+            for c in g:
+                if out[c] < g[c]:
+                    out[c] = g[c]
+        return out
+
+    def power_of_cubes(self) -> int:
+        return reduce(lambda x, y: x * y, self.get_minimal_set().values())
 
 
 def get_games(file_name) -> list[Game]:
@@ -39,14 +51,22 @@ def get_sum_of_valid_games(games: list[Game]) -> int:
     return sum(g.game_number for g in games if g.is_valid())
 
 
+def get_sum_of_power_cubes_from_games(games: list[Game]) -> int:
+    return sum(g.power_of_cubes() for g in games)
+
+
 def main():
     games = get_games("2023/02_test_input")
     part_1 = get_sum_of_valid_games(games)
+    part_2 = get_sum_of_power_cubes_from_games(games)
     print(f"test part1: {part_1}")
+    print(f"test part2: {part_2}")
 
     games = get_games("2023/02_input")
     part_1 = get_sum_of_valid_games(games)
+    part_2 = get_sum_of_power_cubes_from_games(games)
     print(f"part1: {part_1}")
+    print(f"part2: {part_2}")
 
 
 if __name__ == "__main__":
